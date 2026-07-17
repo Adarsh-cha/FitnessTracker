a33a32f6e63b319dfaa2457a85cf229720b91c07
Adarsh <Adarsh@gmail.com>
2026-07-18 02:24:30 +0530

    Add user registration API

---
+++ src/main/java/com/Project/fitness/service/UserService.java
@@ -0,0 +1,17 @@
+package com.Project.fitness.service;
+
+import com.Project.fitness.model.User;
+import com.Project.fitness.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User register(User user) {
+        return userRepository.save(user);
+    }
+}
